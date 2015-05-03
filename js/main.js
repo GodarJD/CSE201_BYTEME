1bcdabc0c18e03919266712371bac2c81672862d
pStraits <pStraits@users.noreply.github.com>
2015-05-03 12:30:05 -0400

    Update main.js
    v2

--- js/main.js
+++ js/main.js
@@ -2,4 +2,18 @@ $(document).ready(function() {
 	$(function () {
 		$('[data-toggle="tooltip"]').tooltip();
 	});
+    var _intervalId;
+    
+    function fadeInLastImg()
+    {
+        var backImg = $('.header img:first');
+        backImg.hide();
+        backImg.remove();
+        $('.container' ).append( backImg );
+        backImg.fadeIn();
+    }
+        
+    _intervalId = setInterval( function() {
+        fadeInLastImg();
+    }, 3000 );
 });
